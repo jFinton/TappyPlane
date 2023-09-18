2715329eb491fc0bcdfc7546a6d1830ae8db5bd6
JessFinton <45701595+jFinton@users.noreply.github.com>
2023-09-18 00:42:34 -0400

    Added Player and Input
    Added Player sprite, Input and Velocity Gravity, including a floor that halts player movement and input

---
+++ plane_cb/plane_cb.gd
@@ -0,0 +1,34 @@
+extends CharacterBody2D
+
+@onready var animation_player = $AnimationPlayer
+@onready var animated_sprite_2d = $AnimatedSprite2D
+
+const GRAVITY: float = 1500.0
+const POWER: float = -400.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	velocity.y += GRAVITY * delta
+	
+	fly()
+	
+	move_and_slide()
+	
+	if is_on_floor() == true:
+		die()
+
+
+func fly() -> void:
+	if Input.is_action_just_pressed("fly") == true:
+		velocity.y = POWER
+		animation_player.play("fly")
+
+
+func die() -> void:
+	animated_sprite_2d.stop() #Stops Animated Sprites Frames
+	set_physics_process(false)
